dd0cb285c297411538369137220816cf476b357f
이나경 <leena3823@gmail.com>
2025-05-30 22:08:51 +0900

    feat: 햄버거 메뉴바 추가

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -24,7 +24,20 @@ export default function Header() {
       className="flex items-center w-full h-[60px] bg-bg-secondary max-w-full 
                    px-[16px] py-[20px] md:px-[24px] md:py-[14px] lg:px-[360px] lg:py-[14px]"
     >
-      <div className="flex items-center gap-10">
+      <div className="flex items-center">
+        {!isLanding && !isAuthPage && mockUser.teams.length > 0 && (
+          <button className="block md:hidden mr-4">
+            <Image
+              src="/icons/icon-gnb_menu.svg"
+              alt="메뉴"
+              width={24}
+              height={24}
+            />
+          </button>
+        )}
+      </div>
+
+      <div className="flex items-center md:gap-8 lg:gap-10">
         <Link href="/">
           <Image
             src="/icons/icon-logo-sm.svg"
@@ -44,27 +57,29 @@ export default function Header() {
 
         {!isLanding && !isAuthPage && (
           <>
-            {mockUser.teams.length > 1 ? (
-              <div className="flex items-center gap-2.5 cursor-pointer">
+            <div className="hidden md:flex items-center md:gap-8 lg:gap-10">
+              {mockUser.teams.length > 1 ? (
+                <div className="flex items-center gap-2.5 cursor-pointer">
+                  <span className="text-text-primary text-md md:text-lg">
+                    {currentTeam}
+                  </span>
+                  <Image
+                    src="/icons/icon-check.svg"
+                    alt="팀 선택"
+                    width={16}
+                    height={16}
+                  />
+                </div>
+              ) : (
                 <span className="text-text-primary text-md md:text-lg">
-                  {currentTeam}
+                  {mockUser.teams[0]}
                 </span>
-                <Image
-                  src="/icons/icon-check.svg"
-                  alt="팀 선택"
-                  width={16}
-                  height={16}
-                />
-              </div>
-            ) : (
-              <span className="text-text-primary text-md md:text-lg">
-                {mockUser.teams[0]}
-              </span>
-            )}
+              )}
 
-            <Link href="/boards" className={"text-md md:text-lg"}>
-              모집게시판
-            </Link>
+              <Link href="/boards" className={"text-md md:text-lg"}>
+                모집게시판
+              </Link>
+            </div>
           </>
         )}
       </div>
@@ -90,7 +105,7 @@ export default function Header() {
                 height={20}
                 className="rounded-full cursor-pointer"
               />
-              <span className="text-sm md:text-base text-text-primary">
+              <span className="text-sm md:text-base text-text-primary hidden lg:inline-block">
                 {mockUser.name}
               </span>
             </Link>
