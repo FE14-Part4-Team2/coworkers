d4a091cc05b96030156db723e857e30489fb78b6
yujin0408 <aszx5849@naver.com>
2025-06-03 23:55:56 +0900

    feat: 비밀번호 노출 토글 구현

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -3,10 +3,18 @@
 import Button from "@/components/common/Button";
 import Input from "@/components/common/Input/Input";
 import PasswordToggle from "@/components/common/Input/PasswordToggle";
+import { useState } from "react";
 
 export default function ResetPasswordPage() {
-  const handlePasswordToggle = () => {};
-  const handleConfirmPasswordToggle = () => {};
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirm, setShowConfirm] = useState(false);
+
+  const handlePasswordToggle = () => {
+    setShowPassword((prev) => !prev);
+  };
+  const handleConfirmPasswordToggle = () => {
+    setShowConfirm((prev) => !prev);
+  };
 
   return (
     <div className="w-full mt-6 sm:mt-0">
@@ -18,20 +26,23 @@ export default function ResetPasswordPage() {
           <Input
             id="password"
             label="새 비밀번호"
-            type="password"
+            type={showPassword ? "text" : "password"}
             placeholder="비밀번호 (영문, 숫자 포함, 12자 이내)를 입력해주세요."
             suffix={
-              <PasswordToggle isShow={false} onToggle={handlePasswordToggle} />
+              <PasswordToggle
+                isShow={showPassword}
+                onToggle={handlePasswordToggle}
+              />
             }
           />
           <Input
             id="comfirmPassword"
             label="비밀번호 확인"
-            type="password"
+            type={showConfirm ? "text" : "password"}
             placeholder="새 비밀번호를 다시 한번 입력해주세요."
             suffix={
               <PasswordToggle
-                isShow={false}
+                isShow={showConfirm}
                 onToggle={handleConfirmPasswordToggle}
               />
             }
