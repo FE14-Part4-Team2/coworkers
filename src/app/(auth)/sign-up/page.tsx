493e450fd0c3582a320d9074a336d5bbe05f2304
yujin0408 <108394961+yujin0408@users.noreply.github.com>
2025-06-06 15:29:30 +0900

    feat: 회원가입 페이지 UI 구현

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,3 +1,18 @@
+import DividerWithText from "@/components/feature/Auth/DividerWithText";
+import KakaoLogin from "@/components/feature/Auth/KakaoLogin";
+import SignupForm from "@/components/feature/Auth/SignupForm/SignupForm";
+
 export default function SignUpPage() {
-  return <div>회원가입 페이지 (준비 중)</div>;
+  return (
+    <div className="w-full mt-5 sm:mt-0">
+      <h2 className="text-center text-text-primary text-2xl md:text-4xl font-medium">
+        회원가입
+      </h2>
+      <div className="mt-6 sm:mt-20">
+        <SignupForm />
+      </div>
+      <DividerWithText />
+      <KakaoLogin message="카카오로 간편 회원가입" />
+    </div>
+  );
 }
