ed2c70cc5c0103a536d5c2c7a7a878ba4013bfac
이나경 <leena3823@gmail.com>
2025-06-10 23:57:25 +0900

    feat: 좋아요 추가/삭제 api 연동

--- src/app/(board)/boards/[articleId]/page.tsx
+++ src/app/(board)/boards/[articleId]/page.tsx
@@ -1,17 +1,22 @@
+"use client";
 import ArticleDetail from "@/components/feature/Boards/Article/ArticleDetail";
 import CommentList from "@/components/feature/Boards/Comment/CommentList";
-import { ArticleMock } from "../ArticleMock";
 import { CommentMock } from "../CommentMock";
 import CommentForm from "@/components/feature/Boards/Comment/CommentForm";
+import { useArticleDetail } from "@/api/article/article.query";
+import { useParams } from "next/navigation";
 
-export default function ArticlePage({
-  params,
-}: {
-  params: { articleId: string };
-}) {
-  const article =
-    ArticleMock.find((item) => item.id === Number(params.articleId)) ||
-    ArticleMock[0];
+export default function ArticlePage() {
+  const { articleId } = useParams();
+  const {
+    data: article,
+    isLoading,
+    error,
+  } = useArticleDetail(articleId as string);
+
+  if (isLoading) return <div>로딩 중...</div>;
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (!article) return <div>게시글을 찾을 수 없습니다.</div>;
 
   return (
     <article className="w-full">
