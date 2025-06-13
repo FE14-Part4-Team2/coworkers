e6bba734bf67ce50412859b6bb6236e081805a67
이나경 <leena3823@gmail.com>
2025-06-13 18:38:02 +0900

    feat: 페이지네이션 공통 button 활용

--- src/components/common/Pagination/Pagination.tsx
+++ src/components/common/Pagination/Pagination.tsx
@@ -1,3 +1,5 @@
+import Button from "../Button";
+
 interface PaginationProps {
   page: number;
   setPage: (page: number) => void;
@@ -16,39 +18,37 @@ export default function Pagination({
 
   return (
     <nav className="flex justify-center items-center gap-2 mt-6 sm:mt-10">
-      <button
+      <Button
+        label="<-"
+        variant="primary"
         onClick={() => setPage(page - 1)}
         disabled={page === 1}
         className="px-3 py-1.5 sm:px-4 sm:py-2 rounded-full bg-bg-tertiary text-bg-inverse border border-brand-primary hover:bg-brand-primary hover:text-text-inverse disabled:opacity-50"
-      >
-        ←
-      </button>
+      />
       {Array.from({ length: totalPages }, (_, idx) => {
         const p = idx + 1;
         const isActive = p === page;
         return (
-          <button
+          <Button
+            label={String(p)}
+            variant={isActive ? "primary" : "ghost"}
             key={p}
             onClick={() => setPage(p)}
-            className={`w-8 h-8 sm:w-10 sm:h-10 rounded-full font-bold border
-                ${
-                  isActive
-                    ? "bg-brand-primary text-text-inverse border-bg-secondary"
-                    : "bg-bg-secondary text-text-inverse border-bg-tertiary hover:bg-brand-primary hover:text-text-inverse"
-                }
+            className={`
+                text-md sm:text-lg w-8 h-8 sm:w-10 sm:h-10 rounded-full font-bold
+                ${isActive ? "border-bg-secondary" : "border-brand-primary"}
               `}
-          >
-            <span className="text-md sm:text-lg">{p}</span>
-          </button>
+            disabled={page === p}
+          />
         );
       })}
-      <button
+      <Button
+        label="->"
+        variant="primary"
         onClick={() => setPage(page + 1)}
         disabled={page === totalPages}
         className="px-3 py-1.5 sm:px-4 sm:py-2 rounded-full bg-bg-tertiary text-text-inverse border border-brand-primary hover:bg-brand-primary hover:text-text-inverse disabled:opacity-50"
-      >
-        →
-      </button>
+      />
     </nav>
   );
 }
