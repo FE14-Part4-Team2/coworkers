08493b207fc0b941d63508441cad8f8d1232a61a
yujin0408 <aszx5849@naver.com>
2025-06-14 16:40:49 +0900

    feat: 랜딩 페이지 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import FeatureSection from "@/components/feature/Landing/FeatureSection";
 import InteractiveGridBg from "@/components/feature/Landing/InteractiveGridBg";
+
 import Image from "next/image";
 import Link from "next/link";
 import { useCallback, useState } from "react";
@@ -33,10 +35,10 @@ export default function Home() {
           onMouseMove={handleMouseMove}
           onMouseLeave={handleMouseLeave}
         ></div>
-        <div className="relative z-10 h-screen">
-          <h1 className="flex flex-wrap justify-center items-center text-2xl  font-semibold">
+        <div className="relative z-10 h-screen text-center">
+          <h1 className="text-2xl sm:text-4xl md:text-[3rem] font-semibold flex justify-center items-center gap-2 flex-wrap">
             함께 만들어가는 투두 리스트
-            <span className=" relative ml-2 w-7 h-7 md:w-12 md:h-12 lg:w-14 lg:h-14">
+            <span className="relative w-7 h-7 md:w-12 md:h-12 lg:w-14 lg:h-14 inline-block">
               <Image
                 src="/icons/icon-repair-lg.svg"
                 alt="망치 아이콘"
@@ -45,7 +47,7 @@ export default function Home() {
               />
             </span>
             <div className="w-full"></div>
-            <span className="mt-1 text-3xl text-transparent bg-clip-text bg-gradient-to-r from-[#10B981] to-[#CEF57E]">
+            <span className="mt-1 text-3xl sm:text-[3rem] md:text-[4rem] text-transparent bg-clip-text bg-gradient-to-r from-[#10B981] to-[#CEF57E] leading-[3.75rem]">
               Coworkers
             </span>
           </h1>
@@ -65,52 +67,20 @@ export default function Home() {
           지금 시작하기
         </Link>
       </div>
-      <div className="px-4">
-        <section className="rounded-[2.5rem] border">
-          <p>
-            <Image
-              src="/icons/landing/icon-landing-folder.svg"
-              alt="폴더 아이콘"
-              width={48}
-              height={48}
-            />
-            그룹으로
-            <br />할 일을 관리해요
-          </p>
-          <div></div>
-          <p>
-            <Image
-              src="/icons/landing/icon-landing-message.svg"
-              alt="메일 아이콘"
-              width={48}
-              height={48}
-            />
-            간단하게 멤버들을 <br />
-            초대해요
-          </p>
-        </section>
-        <section>
-          <div></div>
-          <p>
-            <Image
-              src="/icons/landing/icon-landing-check.svg"
-              alt="체크 아이콘"
-              width={48}
-              height={48}
-            />
-            할 일도 간편하게 <br />
-            체크해요
-          </p>
-        </section>
+      <div>
+        <FeatureSection />
       </div>
 
-      <div>
-        <p>지금 바로 시작해보세요</p>
-        <p>
-          팀원 모두와 같은 방향, <br />
+      <div className="mt-[20rem] pb-[8rem] text-center">
+        <p className="text-2xl font-semibold sm:text-4xl">
+          지금 바로 시작해보세요
+        </p>
+        <p className="mt-3 text-sm font-medium sm:text-2xl sm:mt-4">
+          팀원 모두와 같은 방향, <br className="sm:hidden" />
           같은 속도로 나아가는 가장 쉬운 방법
         </p>
       </div>
+      <div className="w-full h-[60vh] bg-cover bg-center bg-no-repeat bg-landing-bottom-mobile sm:bg-landing-bottom-tablet md:bg-landing-bottom-pc"></div>
     </main>
   );
 }
