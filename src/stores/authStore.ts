8eb93ef6ee6f46e6eb06907dbca499f2217876df
이나경 <leena3823@gmail.com>
2025-06-19 00:38:23 +0900

    fix: useAuthStore isLoading 제거

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -4,7 +4,6 @@ import { create } from "zustand";
 interface AuthState {
   user: UserType | null | undefined;
   isAuthenticated: boolean;
-  isLoading: boolean;
   setAuth: (user: UserType) => void;
   clearAuth: () => void;
 }
@@ -12,7 +11,6 @@ interface AuthState {
 export const useAuthStore = create<AuthState>((set) => ({
   user: undefined,
   isAuthenticated: false,
-  isLoading: true,
   setAuth: (user) => set({ user, isAuthenticated: true }),
   clearAuth: () => set({ user: undefined, isAuthenticated: false }),
 }));
