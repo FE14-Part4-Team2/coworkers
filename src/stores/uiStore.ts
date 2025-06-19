8b1e8d1a788d43aaa04b73df008ac26a9f2a6bce
이나경 <leena3823@gmail.com>
2025-06-19 14:53:41 +0900

    chore: 필요없는 index.ts 삭제

--- src/stores/uiStore.ts
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand";
-
-export const useUiStore = create((set) => ({
-  isUserDropdownOpen: false,
-  setUserDropdownOpen: (isOpen: boolean) => set({ isUserDropdownOpen: isOpen }),
-}));
