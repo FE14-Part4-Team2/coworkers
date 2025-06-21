d6735a4e7c54c7ad8284345503cae8f72d4a1a71
오수빈 Subin OH <almighty5@snu.ac.kr>
2025-06-22 03:34:56 +0900

    feat: 팀 페이지, 초대 페이지 에러 바운더리 적용

--- src/app/(workspace)/[teamId]/page.tsx
+++ src/app/(workspace)/[teamId]/page.tsx
@@ -28,6 +28,8 @@ import TodoEditModal from "@/components/common/Modal/TodoEditModal";
 import { groupService } from "@/api/group/group.service";
 import Skeleton from "@/components/common/Skeleton/Skeleton";
 import SuccessInviteModal from "@/components/common/Modal/SuccessInviteModal";
+import AsyncBoundary from "@/components/common/AsyncBoundary/AsyncBoundary";
+import ErrorFallback from "@/components/common/AsyncBoundary/ErrorFallback";
 
 export default function DashboardPage() {
   const router = useRouter();
@@ -245,197 +247,213 @@ export default function DashboardPage() {
   };
 
   return (
-    <div className="w-full flex-col flex gap-12">
-      <TeamBar teamName={groupData.name} showDropdown={isCurrentAdmin} />
-      <div className="flex flex-col gap-4">
-        <Label
-          title="할 일 목록"
-          count={`(${taskListCount}개)`}
-          modalButton="+ 목록 추가하기"
-          onClickModalButton={() => openModal("todo")}
+    <AsyncBoundary
+      fallback={<></>}
+      errorFallback={({ error, resetErrorBoundary }) => (
+        <ErrorFallback
+          error={error}
+          onRetry={resetErrorBoundary}
+          title="팀 페이지를 불러올 수 없습니다."
         />
-        {taskListCount === 0 ? (
-          <div className="w-full h-32 flex items-center justify-center">
-            <div className="text-text-default text-md">
-              아직 할 일 목록이 없습니다.
+      )}
+    >
+      <div className="w-full flex-col flex gap-12">
+        <TeamBar teamName={groupData.name} showDropdown={isCurrentAdmin} />
+        <div className="flex flex-col gap-4">
+          <Label
+            title="할 일 목록"
+            count={`(${taskListCount}개)`}
+            modalButton="+ 목록 추가하기"
+            onClickModalButton={() => openModal("todo")}
+          />
+          {taskListCount === 0 ? (
+            <div className="w-full h-32 flex items-center justify-center">
+              <div className="text-text-default text-md">
+                아직 할 일 목록이 없습니다.
+              </div>
+            </div>
+          ) : (
+            groupData.taskLists.map((list, index) => {
+              const doneCount = list.tasks.filter(
+                (task: TaskDetailType) => task.doneAt !== null,
+              ).length;
+              const totalCount = list.tasks.length;
+              const colorClass = pointColors[index % pointColors.length];
+
+              return (
+                <div
+                  key={list.id}
+                  onClick={(e) => {
+                    if (modalType) return;
+                    const target = e.target as HTMLElement;
+                    if (target.closest(".prevent-list-click")) return;
+                    router.push(`/${teamId}/task-lists/${list.id}`);
+                  }}
+                >
+                  <ListBar
+                    listName={list.name}
+                    done={doneCount}
+                    total={totalCount}
+                    colorClass={colorClass}
+                    onEdit={() => {
+                      setEditList({ id: list.id.toString(), name: list.name });
+                      openModal("todo-edit");
+                    }}
+                    onDelete={() => {
+                      setDeleteList({
+                        id: list.id.toString(),
+                        name: list.name,
+                      });
+                      setMemberToDelete(null);
+                      openModal("delete");
+                    }}
+                  />
+                </div>
+              );
+            })
+          )}
+        </div>
+        <div className="flex flex-col gap-4">
+          <Label title="리포트" />
+          <div className="w-full flex bg-bg-secondary h-[13.5625rem] p-6 pl-6 sm:pl-8 rounded-xl items-center justify-between gap-6 sm:gap-14">
+            <ReportChart done={todayDoneCount} total={todayTotalCount} />
+            <div className="flex flex-col w-full gap-4 items-end">
+              <ReportBox
+                content="오늘의 할 일"
+                count={todayTotalCount}
+                image="/icons/icon-avatar.svg"
+              />
+              <ReportBox
+                content="한 일"
+                count={todayDoneCount}
+                image="/icons/icon-done.svg"
+              />
             </div>
           </div>
-        ) : (
-          groupData.taskLists.map((list, index) => {
-            const doneCount = list.tasks.filter(
-              (task: TaskDetailType) => task.doneAt !== null,
-            ).length;
-            const totalCount = list.tasks.length;
-            const colorClass = pointColors[index % pointColors.length];
+        </div>
+        <div className="flex flex-col gap-4">
+          <Label
+            title="멤버"
+            count={`(${memberCount}명)`}
+            modalButton="+ 새로운 멤버 초대하기"
+            showModalButton={isCurrentAdmin}
+            onClickModalButton={() => openModal("invite")}
+          />
+          <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 sm:gap-6">
+            {groupData.members.map((member) => {
+              const isSelf = member.userEmail === currentUserEmail;
+              const isAbleButton = isCurrentAdmin ? !isSelf : isSelf;
 
-            return (
-              <div
-                key={list.id}
-                onClick={(e) => {
-                  if (modalType) return;
-                  const target = e.target as HTMLElement;
-                  if (target.closest(".prevent-list-click")) return;
-                  router.push(`/${teamId}/task-lists/${list.id}`);
-                }}
-              >
-                <ListBar
-                  listName={list.name}
-                  done={doneCount}
-                  total={totalCount}
-                  colorClass={colorClass}
-                  onEdit={() => {
-                    setEditList({ id: list.id.toString(), name: list.name });
-                    openModal("todo-edit");
+              return (
+                <MemberBox
+                  key={member.userId + member.userEmail}
+                  profile={
+                    member.userImage || "/icons/icon-profile-default.svg"
+                  }
+                  name={member.userName}
+                  email={member.userEmail}
+                  isAdmin={member.role === "ADMIN"}
+                  isSelf={isSelf}
+                  isAbleButton={isAbleButton}
+                  onClickProfile={() => {
+                    setSelectedProfile({
+                      name: member.userName,
+                      email: member.userEmail,
+                      profileImageUrl: member.userImage || "",
+                    });
+                    openModal("profile");
                   }}
-                  onDelete={() => {
-                    setDeleteList({ id: list.id.toString(), name: list.name });
-                    setMemberToDelete(null);
+                  onClickDelete={() => {
+                    setMemberToDelete({
+                      userId: member.userId,
+                      name: member.userName,
+                      email: member.userEmail,
+                    });
+                    setDeleteList(null);
                     openModal("delete");
                   }}
                 />
-              </div>
-            );
-          })
-        )}
-      </div>
-      <div className="flex flex-col gap-4">
-        <Label title="리포트" />
-        <div className="w-full flex bg-bg-secondary h-[13.5625rem] p-6 pl-6 sm:pl-8 rounded-xl items-center justify-between gap-6 sm:gap-14">
-          <ReportChart done={todayDoneCount} total={todayTotalCount} />
-          <div className="flex flex-col w-full gap-4 items-end">
-            <ReportBox
-              content="오늘의 할 일"
-              count={todayTotalCount}
-              image="/icons/icon-avatar.svg"
-            />
-            <ReportBox
-              content="한 일"
-              count={todayDoneCount}
-              image="/icons/icon-done.svg"
-            />
+              );
+            })}
           </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-4">
-        <Label
-          title="멤버"
-          count={`(${memberCount}명)`}
-          modalButton="+ 새로운 멤버 초대하기"
-          showModalButton={isCurrentAdmin}
-          onClickModalButton={() => openModal("invite")}
-        />
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 sm:gap-6">
-          {groupData.members.map((member) => {
-            const isSelf = member.userEmail === currentUserEmail;
-            const isAbleButton = isCurrentAdmin ? !isSelf : isSelf;
-
-            return (
-              <MemberBox
-                key={member.userId + member.userEmail}
-                profile={member.userImage || "/icons/icon-profile-default.svg"}
-                name={member.userName}
-                email={member.userEmail}
-                isAdmin={member.role === "ADMIN"}
-                isSelf={isSelf}
-                isAbleButton={isAbleButton}
-                onClickProfile={() => {
-                  setSelectedProfile({
-                    name: member.userName,
-                    email: member.userEmail,
-                    profileImageUrl: member.userImage || "",
-                  });
-                  openModal("profile");
-                }}
-                onClickDelete={() => {
-                  setMemberToDelete({
-                    userId: member.userId,
-                    name: member.userName,
-                    email: member.userEmail,
-                  });
-                  setDeleteList(null);
-                  openModal("delete");
-                }}
-              />
-            );
-          })}
-        </div>
-      </div>
-      <TodoEditModal
-        value={editList?.name || ""}
-        onChange={(e) =>
-          setEditList((prev) =>
-            prev ? { ...prev, name: e.target.value } : prev,
-          )
-        }
-        onSubmit={handleEditSubmit}
-      />
-      {modalType === "delete" &&
-        (deleteList ? (
-          <DeleteModal
-            title={`\"${deleteList?.name}"을(를) 삭제하시겠습니까?`}
-            description={`해당 할 일 목록에 포함된 할 일 리스트가 \n 모두 삭제됩니다.`}
-            onConfirm={handleListDelete}
-          />
-        ) : memberToDelete ? (
-          <DeleteModal
-            title={`\"${memberToDelete.name}" 님을 내보내시겠습니까?`}
-            description={`해당 멤버에 관한 모든 정보가 팀에서 삭제되며 \n 본인을 팀에서 삭제할 경우 접근이 불가능합니다.`}
-            onConfirm={handleMemberDelete}
-          />
-        ) : null)}
-      {selectedProfile && (
-        <ProfileModal
-          name={selectedProfile.name}
-          email={selectedProfile.email}
-          profileImageUrl={selectedProfile.profileImageUrl}
-          isSelf={selectedProfile.email === currentUserEmail}
-          onCopy={() => {
-            navigator.clipboard.writeText(selectedProfile.email);
-            closeModal();
-            setSelectedProfile(null);
-            showToast("이메일 복사 완료!", "success");
-          }}
-          onClickUserSetting={() => {
-            closeModal();
-            router.push("/user-setting");
-          }}
-        />
-      )}
-      <TodoModal
-        value={todoTitle}
-        onChange={(e) => setTodoTitle(e.target.value)}
-        onSubmit={(e) => {
-          e.preventDefault();
-          const trimmedTitle = todoTitle.trim();
-          if (!trimmedTitle || !groupData) return;
-
-          const isDuplicate = groupData.taskLists.some(
-            (list) => list.name.trim() === trimmedTitle,
-          );
-
-          if (isDuplicate) {
-            showToast("이미 해당 목록이 존재합니다.", "error");
-            return;
+        <TodoEditModal
+          value={editList?.name || ""}
+          onChange={(e) =>
+            setEditList((prev) =>
+              prev ? { ...prev, name: e.target.value } : prev,
+            )
           }
+          onSubmit={handleEditSubmit}
+        />
+        {modalType === "delete" &&
+          (deleteList ? (
+            <DeleteModal
+              title={`\"${deleteList?.name}"을(를) 삭제하시겠습니까?`}
+              description={`해당 할 일 목록에 포함된 할 일 리스트가 \n 모두 삭제됩니다.`}
+              onConfirm={handleListDelete}
+            />
+          ) : memberToDelete ? (
+            <DeleteModal
+              title={`\"${memberToDelete.name}" 님을 내보내시겠습니까?`}
+              description={`해당 멤버에 관한 모든 정보가 팀에서 삭제되며 \n 본인을 팀에서 삭제할 경우 접근이 불가능합니다.`}
+              onConfirm={handleMemberDelete}
+            />
+          ) : null)}
+        {selectedProfile && (
+          <ProfileModal
+            name={selectedProfile.name}
+            email={selectedProfile.email}
+            profileImageUrl={selectedProfile.profileImageUrl}
+            isSelf={selectedProfile.email === currentUserEmail}
+            onCopy={() => {
+              navigator.clipboard.writeText(selectedProfile.email);
+              closeModal();
+              setSelectedProfile(null);
+              showToast("이메일 복사 완료!", "success");
+            }}
+            onClickUserSetting={() => {
+              closeModal();
+              router.push("/user-setting");
+            }}
+          />
+        )}
+        <TodoModal
+          value={todoTitle}
+          onChange={(e) => setTodoTitle(e.target.value)}
+          onSubmit={(e) => {
+            e.preventDefault();
+            const trimmedTitle = todoTitle.trim();
+            if (!trimmedTitle || !groupData) return;
 
-          createTaskList(
-            { name: todoTitle.trim() },
-            {
-              onSuccess: () => {
-                setTodoTitle("");
-                closeModal();
-                showToast("목록 추가 완료!", "success");
+            const isDuplicate = groupData.taskLists.some(
+              (list) => list.name.trim() === trimmedTitle,
+            );
+
+            if (isDuplicate) {
+              showToast("이미 해당 목록이 존재합니다.", "error");
+              return;
+            }
+
+            createTaskList(
+              { name: todoTitle.trim() },
+              {
+                onSuccess: () => {
+                  setTodoTitle("");
+                  closeModal();
+                  showToast("목록 추가 완료!", "success");
+                },
+                onError: () => {
+                  closeModal();
+                  showToast("목록 추가 실패", "error");
+                },
               },
-              onError: () => {
-                closeModal();
-                showToast("목록 추가 실패", "error");
-              },
-            },
-          );
-        }}
-      />
-      <InviteModal onCopy={handleCopyInviteLink} />
-      <SuccessInviteModal />
-    </div>
+            );
+          }}
+        />
+        <InviteModal onCopy={handleCopyInviteLink} />
+        <SuccessInviteModal />
+      </div>
+    </AsyncBoundary>
   );
 }
